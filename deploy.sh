4880bf92ba135ef47820d3a6e6530d1e9411fcd5
Jose Molina <jose.molina@cern.ch>
2015-11-12 17:51:19 +0100

    Minor changes

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-set -x
-
 cp cvmfs_web/settings.py.example cvmfs_web/settings.py
 cp django.conf /etc/httpd/conf.d/django.conf
 
 rm -rf /run/httpd/ /tmp/httpd
 mkdir -p /run/httpd /tmp/httpd
 
-touch /var/log/httpd/access_log
-tail -f /var/log/httpd/access_log &
 exec /usr/sbin/apachectl -DFOREGROUND
-
