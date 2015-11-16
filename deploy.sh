8f7d97b7b7e560ca073afc719a1a92d1d937513f
Jose Molina <jose.molina@cern.ch>
2015-11-16 16:00:14 +0100

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+rm -f cvmfs_web/settings.py
 cp cvmfs_web/settings.py.example cvmfs_web/settings.py
 
 rm -rf /run/httpd/ /tmp/httpd
 mkdir -p /run/httpd /tmp/httpd
+
+python manage.py collectstatic --noinput
+chown -R apache /var/www/html
 
 exec /usr/sbin/apachectl -DFOREGROUND
--- start_server.sh
+++ start_server.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$( cd $(dirname $0) ; pwd -P )
 
-docker build -t monitor "$SCRIPT_DIR"
+docker build -t monitor $@ "$SCRIPT_DIR"
 docker run -p 80:80 -p 443:443 -v "$SCRIPT_DIR":/opt/django-cvmfs-monitor:rw monitor
